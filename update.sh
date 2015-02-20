92fb0ec67d6891b4505df27c3e45a3395457bc65
Matt Fischer <matt@mattfischer.com>
2015-02-20 04:09:07 +0000

    adding more services

--- update.sh
+++ update.sh
@@ -5,45 +5,78 @@
 # libxml2-dev libxslt-dev libffi-dev python-libvirt pkg-config
 # libvirt-dev
 
-# heat is broken as of Feb 19 2015
-# neutron doesn't seem to have the option at all
-
-SERVICES="nova cinder glance ceilometer"
-BRANCHES="master"
+GEN_SERVICES="nova cinder glance ceilometer heat"
+PREBUILT_SERVICES="keystone designate neutron"
+BRANCH="master"
 
 set -x
-#set -e
 
-for SERVICE in ""${SERVICES}""
-do
+setup()
+{
+    SERVICE=$1
     cd ~/openstack/${SERVICE}
     git remote update
-    for BRANCH in ""${BRANCHES}""
-    do
-        echo "Generating ${BRANCH} for ${SERVICE}"
-        git checkout ${BRANCH}
-        git reset origin/${BRANCH}
-        git pull
-        rm -rf .venv
-        tox -egenconfig
-        RELEASE=`basename ${BRANCH}`
-        LOG_SUFFIX=`git log -n1 --oneline`
-        echo "Last commit for ${SERVICE} is ${LOG_SUFFIX}"
-        cp etc/${SERVICE}/${SERVICE}*conf.sample ~/ossc/samples/
-        # glance is special for some annoying reason
-        if [ "${SERVICE}" == "glance" ]; then
-            cp etc/${SERVICE}*conf.sample ~/ossc/samples/
-        fi
-        # ceilometer is also a snowflake, lots of snowflakes...
-        if [ "${SERVICE}" == "ceilometer" ]; then
-            cp etc/${SERVICE}/${SERVICE}.conf ~/ossc/samples/
-        fi
+    echo "Generating ${BRANCH} for ${SERVICE}"
+    git checkout ${BRANCH}
+    git reset origin/${BRANCH}
+    git pull
+}
 
-        cd ~/ossc
-        git add samples
-        git commit -a -m "${SERVICE}: ${LOG_SUFFIX}"
-    done
+commit()
+{
+    SERVICE=$1
+    cd ~/ossc
+#    git add samples
+#    git commit -a -m "${SERVICE}: ${LOG_SUFFIX}"
+}
+
+for SERVICE in ""${PREBUILT_SERVICES}""
+do
+    setup ${SERVICE}
+    RELEASE=`basename ${BRANCH}`
+    LOG_SUFFIX=`git log -n1 --oneline`
+    echo "Last commit for ${SERVICE} is ${LOG_SUFFIX}"
+
+    if [ "${SERVICE}" == "neutron" ]; then
+        cp etc/${SERVICE}.conf ~/ossc/samples/
+        cp etc/l3_agent.ini ~/ossc/samples/
+        cp etc/metadata_agent.ini ~/ossc/samples/
+        cp etc/dhcp_agent.ini ~/ossc/samples/
+    else
+        cp etc/${SERVICE}/${SERVICE}*conf.sample ~/ossc/samples/
+    fi
+
+    commit ${SERVICE}
+done
+
+for SERVICE in ""${GEN_SERVICES}""
+do
+    setup ${SERVICE}
+    rm -rf .venv
+    tox -egenconfig
+    RELEASE=`basename ${BRANCH}`
+    LOG_SUFFIX=`git log -n1 --oneline`
+    echo "Last commit for ${SERVICE} is ${LOG_SUFFIX}"
+    cp etc/${SERVICE}/${SERVICE}*conf.sample ~/ossc/samples/
+
+    # glance is special for some annoying reason
+    if [ "${SERVICE}" == "glance" ]; then
+        cp etc/${SERVICE}*conf.sample ~/ossc/samples/
+    fi
+
+    # ceilometer is also a snowflake, lots of snowflakes...
+    if [ "${SERVICE}" == "ceilometer" ]; then
+        cp etc/${SERVICE}/${SERVICE}.conf ~/ossc/samples/
+    fi
+
+    # heat broken: https://bugs.launchpad.net/heat/+bug/1412571
+    if [ "${SERVICE}" == "heat" ]; then
+        echo "heat is busted"
+    fi
+
+    commit ${SERVICE}
+
 done
 echo "Pushing"
 cd ~/ossc
-git push -u origin master
+#git push -u origin master
