a7d57d4139a6944a3b6ff589cb847d62b7fde54e
Matt Fischer <matt@mattfischer.com>
2015-02-20 03:14:57 +0000

    noting that heat is broken
    make glance a special case because it's special

--- update.sh
+++ update.sh
@@ -5,7 +5,10 @@
 # libxml2-dev libxslt-dev libffi-dev python-libvirt pkg-config
 # libvirt-dev
 
-SERVICES="nova cinder glance heat"
+# heat is broken as of Feb 19 2015
+# neutron doesn't seem to have the option at all
+
+SERVICES="nova cinder glance"
 BRANCHES="master"
 
 set -x
@@ -27,6 +30,10 @@ do
         LOG_SUFFIX=`git log -n1 --oneline`
         echo "Last commit for ${SERVICE} is ${LOG_SUFFIX}"
         cp etc/${SERVICE}/${SERVICE}*conf.sample ~/ossc/samples/
+        # glance is special for some annoying reason
+        if [ ${SERVICE} -eq "glance" ]; then
+            cp etc/${SERVICE}*conf.sample ~/ossc/samples/
+        fi
         cd ~/ossc
         git add samples
         git commit -a -m "${SERVICE}: ${LOG_SUFFIX}"
@@ -34,4 +41,4 @@ do
 done
 echo "Pushing"
 cd ~/ossc
-#git push -u origin master
+git push -u origin master
